876b0a1785ee1ad46a0dc4502d10dfcec51bc253
Andrew McNeill <andrew@andrewmc.ca>
2022-02-10 18:59:08 -0400

    Completes LighthouseBnB Add Property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -61,14 +61,6 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
-
-  const name = user.name;
-  const email = user.email;
-  const password = user.password;
 
   return pool.query(
     `INSERT INTO users
@@ -122,14 +114,6 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function(options, limit = 10) {
 
-  // return pool.query(
-  //   `SELECT *
-  //   FROM properties
-  //   LIMIT $1`,
-  //   [`${limit}`])
-  //   .then((result) => result.rows)
-  //   .catch((err) => err.message);
-
   const queryParams = [];
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
@@ -182,9 +166,19 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  
+  return pool.query(
+    `INSERT INTO properties
+    VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, TRUE)
+    RETURNING *;`,
+    [`${property.owner_id}`, `${property.title}`, `${property.description}`, `${property.thumbnail_photo_url}`, `${property.cover_photo_url}`, `${property.cost_per_night}`, `${property.parking_spaces}`, `${property.number_of_bathrooms}`, `${property.number_of_bedrooms}`, `${property.country}`, `${property.street}`, `${property.city}`, `${property.province}`, `${property.post_code}`])
+    .then((result) => {
+      if (result.rows) return result.rows[0];
+      return null;
+    })
+    .catch((err) => err.message);
+
+  
+  console.log(property);
 };
 exports.addProperty = addProperty;
